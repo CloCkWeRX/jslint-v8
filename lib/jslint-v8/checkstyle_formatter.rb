acce978ad1687a5601186494072a7a17f21864bc
Daniel O'Connor <daniel.oconnor@gmail.com>
2012-08-16 09:31:23 +0930

    Add a checkstyle formatter

---
+++ lib/jslint-v8/checkstyle_formatter.rb
@@ -0,0 +1,46 @@
+
+module JSLintV8
+   class CheckstyleFormatter < Formatter
+      attr_reader :output_stream
+    
+      def initialize(stream)
+         @output_stream = stream
+      end
+
+      def tick(errors)
+         output_stream.flush
+      end
+
+      def summary(tested_files, lint_result)
+         output_stream.print '<?xml version="1.0" encoding="utf-8"?>' + "\n"
+         output_stream.print '<checkstyle>' + "\n"
+         if lint_result.keys.any?
+            print_error_summary(lint_result)
+            output_stream.print "\n"
+         end
+         output_stream.print '/<checkstyle>' + "\n"
+         
+      end
+
+   private
+      def print_error_summary(result) 
+         out = output_stream
+
+         # we iterate the sorted keys to prevent a brittle test and also the output
+         # should be nicer as it will be guaranteed to be alphabetized
+         result.keys.sort.each do |file|
+            errors = result[file]
+
+            #out.print "#{file}:\n"
+            out.print "<file name=\"#{file}\">\n"
+            errors.each do |error|
+               out.print "\t<error line=\"#{error.line_number}\" column=\"#{error.character}\" severity=\"warning\" message=\"#{error.reason}\" source=\"net.csslint.Catfish\" />\n"
+
+               #source=\"" + generateSource(message.rule) +"\"
+               # net.csslint.' + rule.name.replace(/\s/g,'')
+            end
+            out.print "</file>"
+         end
+      end
+   end
+end
